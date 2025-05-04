9c672d868cce3955059d9b1925baf3959cec18fe
bhavyaajainn <103187367+bhavyaajainn@users.noreply.github.com>
2025-05-04 12:21:08 +0530

    removed extra files

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,17 +1,15 @@
+"use client"; // Mark this file as a Client Component
+
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
-import { Providers } from "./providers";
+import { Provider } from "react-redux";
+import { store } from "@/app/store";
 
 const geistSans = Poppins({
   subsets: ["latin"],
-  weight: ["400", "500", "600"]
+  weight: ["400", "500", "600"],
 });
-
-export const metadata: Metadata = {
-  title: "Finlearn",
-  description: "Learn money. Live smart. Every day, with FinLearn AI.",
-};
 
 export default function RootLayout({
   children,
@@ -21,9 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.className} antialiased`}>
-        <Providers>
+        <Provider store={store}>
           {children}
-        </Providers>
+        </Provider>
       </body>
     </html>
   );
---
+++ frontend/app/metadata.ts
@@ -0,0 +1,6 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Finlearn",
+  description: "Learn money. Live smart. Every day, with FinLearn AI.",
+};
